fd43aa026018db63f6d0713ccc465ac6914e1b33
Brady Law <bradyl16@gmail.com>
2010-04-21 13:23:17 -0400

    Load routes from SQLite file

--- ShuttleTrac/Classes/ShuttleTracDataStore.m
+++ ShuttleTrac/Classes/ShuttleTracDataStore.m
@@ -22,21 +22,14 @@
 
 -(id)init {
 	if (self = [super init]) {
-		// FIXME
-		CLLocationCoordinate2D loc;
-		loc.latitude = 0;
-		loc.longitude = 0;
-		/*
-		busStops = [[NSArray arrayWithObject:
-					[BusStop busStopWithName:@"Courtyards" stopNumber:1 location:loc]] retain];
-		*/
 		busStops = [[NSMutableArray alloc] init];
-		busRoutes = [[NSArray arrayWithObject:
-					 [BusRoute busRouteWithID:1 name:@"Purple" stops:nil]] retain];
+		busRoutes = [[NSMutableArray alloc] init];
 		
 		NSString *databasePath = [[NSBundle mainBundle] pathForResource:@"shuttleTracDataStore" ofType:@"sqlite"];
 		
 		if(sqlite3_open([databasePath UTF8String], &database) == SQLITE_OK) {
+			
+			// Get all stops from SQLite file
 			sqlite3_stmt *compiledStatement;
 			const char *sqlStatement = "SELECT id,name,latitude,longitude FROM stops;";
 			if (sqlite3_prepare(database, sqlStatement, -1, &compiledStatement, NULL) == SQLITE_OK) {
@@ -44,12 +37,24 @@
 					CLLocationCoordinate2D loc = {sqlite3_column_double(compiledStatement, 2), sqlite3_column_double(compiledStatement, 3)};
 					const char *name = (const char *) sqlite3_column_text(compiledStatement, 1);
 					
-					[busStops addObject:[BusStop 
-										 busStopWithName:[NSString stringWithUTF8String:name]
-										 stopNumber:sqlite3_column_int(compiledStatement, 0)
-										 location:loc]];
+					[busStops addObject:[BusStop busStopWithName:[NSString stringWithUTF8String:name]
+													  stopNumber:sqlite3_column_int(compiledStatement, 0)
+														location:loc]];
 				}
 			}
+			
+			// Get all routes from SQLite file
+			const char *sqlStatement2 = "SELECT id,name,stops FROM routes;";
+			if (sqlite3_prepare(database, sqlStatement2, -1, &compiledStatement, NULL) == SQLITE_OK) {
+				while (sqlite3_step(compiledStatement) == SQLITE_ROW) {
+					const char *name = (const char *) sqlite3_column_text(compiledStatement, 1);
+					
+					[busRoutes addObject:[BusRoute busRouteWithID:sqlite3_column_int(compiledStatement, 0) 
+															 name:[NSString stringWithUTF8String:name] 
+															stops:nil]];
+				}
+			}
+			
 		}
 	}
 	
