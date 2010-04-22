ec0268164617e01b9bc30719151527e94144526f
Brady Law <bradyl16@gmail.com>
2010-04-21 23:54:22 -0400

    Auto zoom for map

--- ShuttleTrac/Classes/BusMapViewController.m
+++ ShuttleTrac/Classes/BusMapViewController.m
@@ -14,6 +14,7 @@
 
 @interface BusMapViewController ( )
 - (void)addBusStops;
+-(void)zoomToFitMapAnnotations;
 @end
 
 
@@ -37,6 +38,9 @@
     [super viewDidLoad];
 	
 	[self addBusStops];
+	
+	// FIXME - We probably shouldn't run this for > 10 stops
+	[self zoomToFitMapAnnotations];
 }
 
 
@@ -68,6 +72,41 @@
 		MKPlacemark *newPlacemark = [MKPlacemark placemarkWithCoordinate:[stop location] addressDictionary:address];
 		[mapView addAnnotation:newPlacemark];
 	}
+}
+
+#pragma mark -
+#pragma mark Auto fit zoom
+
+-(void)zoomToFitMapAnnotations
+{
+    if([mapView.annotations count] == 0)
+        return;
+	
+    CLLocationCoordinate2D topLeftCoord;
+    topLeftCoord.latitude = -90;
+    topLeftCoord.longitude = 180;
+	
+    CLLocationCoordinate2D bottomRightCoord;
+    bottomRightCoord.latitude = 90;
+    bottomRightCoord.longitude = -180;
+	
+    for(id <MKAnnotation> annotation in mapView.annotations)
+    {
+        topLeftCoord.longitude = fmin(topLeftCoord.longitude, annotation.coordinate.longitude);
+        topLeftCoord.latitude = fmax(topLeftCoord.latitude, annotation.coordinate.latitude);
+		
+        bottomRightCoord.longitude = fmax(bottomRightCoord.longitude, annotation.coordinate.longitude);
+        bottomRightCoord.latitude = fmin(bottomRightCoord.latitude, annotation.coordinate.latitude);
+    }
+	
+    MKCoordinateRegion region;
+    region.center.latitude = topLeftCoord.latitude - (topLeftCoord.latitude - bottomRightCoord.latitude) * 0.5;
+    region.center.longitude = topLeftCoord.longitude + (bottomRightCoord.longitude - topLeftCoord.longitude) * 0.5;
+    region.span.latitudeDelta = fabs(topLeftCoord.latitude - bottomRightCoord.latitude) * 1.1; // Add a little extra space on the sides
+    region.span.longitudeDelta = fabs(bottomRightCoord.longitude - topLeftCoord.longitude) * 1.1; // Add a little extra space on the sides
+	
+    region = [mapView regionThatFits:region];
+    [mapView setRegion:region animated:YES];
 }
 
 #pragma mark -
