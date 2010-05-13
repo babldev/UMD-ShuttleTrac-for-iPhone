34e309acc72b36edaf4bbbb5b90aadf0812bb6c4
Brady Law <bradyl16@gmail.com>
2010-05-13 17:12:15 -0400

    encodeObject fix

--- ShuttleTrac/Classes/BusStopArrivals.m
+++ ShuttleTrac/Classes/BusStopArrivals.m
@@ -50,9 +50,7 @@
 		
 		stop = [[coder decodeObjectForKey:@"stop"] retain];
 		route = [[coder decodeObjectForKey:@"route"]retain];
-		
-		delegate = [[coder decodeObjectForKey:@"delegate"] retain];
-		
+				
 		upcomingBusRoutes = [[coder decodeObjectForKey:@"upcomingBusRoutes"]retain];
 		lastRefresh = [[coder decodeObjectForKey:@"lastRefresh"] retain];
 	}
@@ -68,8 +66,6 @@
 	
 	[coder encodeObject:upcomingBusRoutes forKey:@"upcomingBusRoutes"];	
 	[coder encodeObject:lastRefresh forKey:@"lastRefresh"];	
-	
-	[coder encodeObject:delegate forKey:@"delegate"];
 }
 
 -(NSString *) getBusStopName{
